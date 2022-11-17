db9964d21f347a4b31ce98ef37b758e0e1fa4e51
sanket1570 <sanket2@pdx.edu>
2022-11-16 20:44:37 -0800

    commit cpp file

--- g22_final_v1.py
+++ g22_final_v1.py
@@ -29,26 +29,26 @@ if file_exists:
             count += 1
             x = line.split(" ")
             print("Reading line:",count,"of trace file -",x[0],x[1])
-            if x[0] == '0':
-                print("Operation - Read request from L1 data cache for address",x[1],end='')
-            elif x[0] == '1':
-                print("Operation - Write request from L1 data cache for address",x[1],end='')
-            elif x[0] == '2':
-                print("Operation - Read request from L1 instruction cache for address",x[1],end='')
-            elif x[0] == '3':
-                print("Operation - snooped invalidate command for address",x[1],end='')
-            elif x[0] == '4':
-                print("Operation - snooped read request for address",x[1],end='')
-            elif x[0] == '5':
-                print("Operation - snooped write request for address",x[1],end='')
-            elif x[0] == '6':
-                print("Operation - snooped read with intent to modify request for address",x[1],end='')
-            elif x[0] == '7':
-                print("Operation - No operation")
-            elif x[0] == '8':
-                print("Operation - clear the cache and reset all state for address",x[1],end='')
-            elif x[0] == '9':
-                print("Operation - print contents and state of each valid cache line for address",x[1],end='')
+            if (x[0] == '0') {
+                print("Operation - Read request from L1 data cache for address",x[1],end='')}
+            else if (x[0] == '1'){
+                print("Operation - Write request from L1 data cache for address",x[1],end='')}
+            else if (x[0] == '2'){
+                print("Operation - Read request from L1 instruction cache for address",x[1],end='')}
+            else if (x[0] == '3'){
+                print("Operation - snooped invalidate command for address",x[1],end='')}
+            else if (x[0] == '4'){
+                print("Operation - snooped read request for address",x[1],end='')}
+            else if (x[0] == '5'){
+                print("Operation - snooped write request for address",x[1],end='')}
+            else if (x[0] == '6'){
+                print("Operation - snooped read with intent to modify request for address",x[1],end='')}
+            else if (x[0] == '7'){
+                print("Operation - No operation")}
+            else if (x[0] == '8'){
+                print("Operation - clear the cache and reset all state for address",x[1],end='')}
+            else if (x[0] == '9'){
+                print("Operation - print contents and state of each valid cache line for address",x[1],end='')}
             print("\n")
     f.close()
     print("%%%%%%%% End of Simulation %%%%%%%%")        
