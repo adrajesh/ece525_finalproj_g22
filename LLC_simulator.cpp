fe17b38cc9d3ef875020cec0900e5a40fc030493
adrajesh <117257733+adrajesh@users.noreply.github.com>
2022-12-01 03:44:49 -0800

    Update LLC_simulator.cpp
    Size not hard coded anymore

--- LLC_simulator.cpp
+++ LLC_simulator.cpp
@@ -14,9 +14,6 @@
 #include "Snoop_Result.cpp"
 using namespace std;
 
-#define temp_ways 8
-#define temp_sets 16384
-
 int offset_b;
 int index_b;
 unsigned int set_mask;
@@ -25,22 +22,24 @@ bool SilentMode;
 bool File;
 int SnoopResult;
 int SnoopResult2;
+int ways;
+int sets;
 
 enum mesi_st {I,E,S,M}; // I-0; E-1; S-2; M-3;
 	
 struct lines {
-	int MESI;												// 2 bits for MESI 
-	unsigned int TAG;										// tag bits
+	int MESI;						// 2 bits for MESI 
+	unsigned int TAG;				// tag bits
 } ;
 	
 struct cset {
-	int PLRU;												// 7 bits for PLRU.
-	lines LINE[temp_ways];									// hard coded, need to check for a way not to hard code.
-} ;															// hard coded, need to check for a way not to hard code.
+	int PLRU;																	// 7 bits for PLRU.
+	struct lines *LINE = (lines*)malloc(sizeof(struct lines) * ways);			// Lines for n-way
+} ;															
 
 int checkWay(cset cache) {
 	int empty_way;
-	for(int i=0;i<temp_ways;i++) {
+	for(int i=0;i<ways;i++) {
 		if (cache.LINE[i].MESI == I) {
 			empty_way = i;
 			break;
@@ -50,19 +49,23 @@ int checkWay(cset cache) {
 		empty_way = 8;
 		cout<<"All ways are full!"<<endl;
 	}
+	if(NormalMode) {
 	cout<<"Empty way - "<<empty_way<<endl;
 	return empty_way;
+	}
 }
 
 int waysFilled(cset cache) {
 int ways_filled = 0;
-	for (int i=0;i<temp_ways;i++) {
+	for (int i=0;i<ways;i++) {
 		if (cache.LINE[i].MESI != I) {
 			ways_filled++;
 		}
 	}
+if(NormalMode) {
 cout<< "Ways filled - " <<ways_filled<<endl;	
 return ways_filled;
+}
 }
 
 // Parse for trace file.
@@ -104,12 +107,8 @@ int main(int argc, char* argv[]) {
 	unsigned int trace[2];
 	int size;
 	int linesize;
-	int ways;
-	int sets;
-//	infile.open ("trace.log");
 	int temp_SET;
 	int temp_TAG;
-	cset CACHE[temp_sets];
 	
 	cout<<endl<<"WELCOME TO LAST LEVEL CACHE"<<endl<<endl;
 	if(argc==1) {											// No Arguments provided, NormalMode (default) and Trace file is trace.log	
@@ -177,6 +176,11 @@ int main(int argc, char* argv[]) {
 	offset_b = log2(linesize);
 	sets = (size * pow(2,20))/(linesize * ways);
 	index_b = log2(sets);
+
+//	cout << "CHECK - "<< ways <<" ~~~ "<<sets<<endl<<endl<<endl;
+	
+	cset CACHE[sets];
+//	struct cset *CACHE = (cset*)malloc(sizeof(struct cset) * sets);			// Dynamic allocation of CACHE size 
 	
 	if(NormalMode) {
 	cout<<"Number of Offset bits: "<<offset_b<<endl;
@@ -187,18 +191,18 @@ int main(int argc, char* argv[]) {
 
 // 	Initializing Cache at start of the run - setting all MESI bits to invalid
 	cout<<"Initializing the Cache!"<<endl;
-	for(int i=0;i<temp_sets;i=i+1){
-		for(int j=0;j<temp_ways;j=j+1){
+	for(int i=0;i<sets;i=i+1){
+		for(int j=0;j<ways;j=j+1){
 			CACHE[i].LINE[j].MESI=I;
 		}
 	}
-/*	cout<<"Done initializing the Cache!!"<<endl;
+	cout<<"Done initializing the Cache!!"<<endl;
 	
-	cout<<"Checking 1 -"<<CACHE[0].LINE[0].MESI<<endl;
+//	cout<<"Checking 1 -"<<CACHE[0].LINE[0].MESI<<endl;
 	
-	if (CACHE[0].LINE[0].MESI == I) {
-		cout<<"Checking 2"<<endl;
-	} */
+//	if (CACHE[0].LINE[0].MESI == I) {
+//		cout<<"Checking 2"<<endl;
+//	} 
 	
 	int curr_way;
 	int ways_filled;
@@ -214,18 +218,17 @@ int main(int argc, char* argv[]) {
 		temp_SET = get_set(address);
 		temp_TAG = get_tag(address);
 		SnoopResult= GetSnoopResult(address);
-		if(SnoopResult==0){              
+		if(SnoopResult==0 && NormalMode){              
 			cout<<"Snoop result: NOHIT"<<endl;
 		}
 
-		else if(SnoopResult==1){         
+		else if(SnoopResult==1 && NormalMode){         
 			cout<<"Snoop result: HIT"<<endl;
 		}
 
-		else if(SnoopResult==2){                             
+		else if(SnoopResult==2 && NormalMode){                             
 			cout<<"Snoop result: HITM"<<endl;
-		}
-		
+		}	
 		
 		ways_filled = waysFilled(CACHE[temp_SET]);
 		
@@ -265,7 +268,7 @@ int main(int argc, char* argv[]) {
 		
 		else if (ways_filled>0 && ways_filled<8) {						// Partially filled. Check both hit or miss.
 			int hit = 0;
-			for(int i=0;i<temp_ways;i++) {
+			for(int i=0;i<ways;i++) {
 				if (CACHE[temp_SET].LINE[i].MESI != I && CACHE[temp_SET].LINE[i].TAG == temp_TAG) {			// Hit
 					if(NormalMode){
 						cout<< "Hit!!! Tag is - "<< CACHE[temp_SET].LINE[i].TAG <<endl;
@@ -311,7 +314,7 @@ int main(int argc, char* argv[]) {
 		}
 		else if (ways_filled == 8) {													// All filled. Either hit or miss. For miss - eviction needed
 			int hit = 0;
-			for(int i=0;i<temp_ways;i++) {
+			for(int i=0;i<ways;i++) {
 				if (CACHE[temp_SET].LINE[i].MESI != I && CACHE[temp_SET].LINE[i].TAG == temp_TAG) {			// Hit
 					if(NormalMode){
 						cout<< "Hit!!! Tag is - "<< CACHE[temp_SET].LINE[i].TAG <<endl;
@@ -399,11 +402,11 @@ int main(int argc, char* argv[]) {
 		}
 	else if(instr==9) {
 		cout<<endl<<"Operation - print contents and state of each valid cache line"<<endl;
-		for(int i=0;i<temp_sets;i=i+1){
-			for(int j=0;j<temp_ways;j=j+1){
+		for(int i=0;i<sets;i=i+1){
+			for(int j=0;j<ways;j=j+1){
 				if (CACHE[i].LINE[j].MESI!=I) {
 					if(NormalMode || SilentMode) {
-						cout<<"Contents of cache at Index "<< i << " way " << j << " is "<<CACHE[i].LINE[j].TAG <<endl;
+						cout<<"Contents of cache(Tag) at Index "<< i << " way " << j << " is "<<CACHE[i].LINE[j].TAG <<endl;
 					}
 				}
 			}
