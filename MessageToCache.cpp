3e17212a0d3cc736842e0479037bc55e39ddc422
adrajesh <117257733+adrajesh@users.noreply.github.com>
2022-12-04 04:14:44 -0800

    Update MessageToCache.cpp

--- MessageToCache.cpp
+++ MessageToCache.cpp
@@ -6,8 +6,22 @@
 #include<cstring>
 using namespace std;
 
+// #define SENDLINE 1
+// #define GETLINE 2
+// #define INVALIDATELINE 3
+// #define EVICTLINE 4
 
 void MessageToCache(int Message, unsigned int address){
-    
-    cout<<"L2 to L1 Message: "<<Message<<", "<<address<<endl;
+	if (Message == 1) {
+		cout<<"L2 to L1 Message: SENDLINE"<<", "<<address<<endl;
+	}
+	else if (Message == 2) {
+		cout<<"L2 to L1 Message: GETLINE"<<", "<<address<<endl;
+	}
+	else if (Message == 3) {
+		cout<<"L2 to L1 Message: INVALIDATELINE"<<", "<<address<<endl;
+	}
+	else if (Message == 4) {
+		cout<<"L2 to L1 Message: EVICTLINE"<<", "<<address<<endl;
+	}
 }
