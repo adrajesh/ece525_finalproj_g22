076d6e889eecf4530a1508846bd9a1d1f7755607
adrajesh <117257733+adrajesh@users.noreply.github.com>
2022-12-04 04:15:05 -0800

    Update PutSnoopResult.cpp

--- PutSnoopResult.cpp
+++ PutSnoopResult.cpp
@@ -6,8 +6,20 @@
 #include<cstring>
 using namespace std;
 
+#define NOHIT 0
+#define HIT 1
+#define HITM 2
+
 /* Report the result of our snooping bus operations performed by other caches */
 void PutSnoopResult(unsigned int Address, int SnoopResult) 
 {
-	cout<<"SnoopResult: "<< SnoopResult << ", Address: " << Address<<endl;
+	if(SnoopResult == 0) {
+	cout<<"SnoopResult: NOHIT"<< ", Address: " << Address<<endl;
+	}
+	if(SnoopResult == 1) {
+	cout<<"SnoopResult: HIT"<< ", Address: " << Address<<endl;
+	}
+	if(SnoopResult == 2) {
+	cout<<"SnoopResult: HITM"<< ", Address: " << Address<<endl;
+	}
 }
