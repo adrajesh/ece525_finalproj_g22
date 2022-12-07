4bff5b094956083bb159b206abd789dab9a0efa9
Sanket Patil <117404301+sanket1570@users.noreply.github.com>
2022-12-06 23:05:24 -0800

    Update MessageToCache.cpp

--- MessageToCache.cpp
+++ MessageToCache.cpp
@@ -11,16 +11,16 @@ extern bool NormalMode;
 void MessageToCache(int Message, unsigned int address){
 	if(NormalMode){
 		if (Message == 1) {
-			cout<<"L2 to L1 Message: GETLINE"<<"\tAddress: "<<address<<endl;
+			cout<<"L2 to L1 Message: GETLINE"<<"\tAddress: "<<std::hex<<address<<endl;
 		}
 		else if (Message == 2) {
-			cout<<"L2 to L1 Message: SENDLINE"<<"\tAddress: "<<address<<endl;
+			cout<<"L2 to L1 Message: SENDLINE"<<"\tAddress: "<<std::hex<<address<<endl;
 		}
 		else if (Message == 3) {
-			cout<<"L2 to L1 Message: INVALIDATELINE"<<"\tAddress: "<<address<<endl;
+			cout<<"L2 to L1 Message: INVALIDATELINE"<<"\tAddress: "<<std::hex<<address<<endl;
 		}
 		else if (Message == 4) {
-			cout<<"L2 to L1 Message: EVICTLINE"<<"\tAddress: "<<address<<endl;
+			cout<<"L2 to L1 Message: EVICTLINE"<<"\tAddress: "<<std::hex<<address<<endl;
 		}
 	}
 	
